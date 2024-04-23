b8603185c86bf35848497eab72f0747873be1ec6
MsalelaA <164764312+MsalelaA@users.noreply.github.com>
2024-04-23 20:37:33 +0200

    Add files via upload

---
+++ greatingcard/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.greatingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greatingcard.ui.theme.GreatingCardTheme
+
+class MainActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContent()
+        {
+            GreatingCardTheme() 
+            {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    Greeting(" AFIKILE")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) 
+{
+    Surface (color = Color.Cyan)
+    {
+        Text(
+            text = "HELLO MY NAME IS $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview()
+{
+    GreatingCardTheme()
+    {
+        Greeting("AFIKILE")
+    }
+}
